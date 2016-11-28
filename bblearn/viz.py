eff29799498f608c8bb12e4a1be4ff452ca73b36
Paolo Montesel <paolo.montesel@gmail.com>
2016-11-28 00:55:36 -0100

    viz: confusion matrix

---
+++ bblearn/viz.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import numpy as np
+from matplotlib import pylab as plt
+from sklearn.metrics import confusion_matrix as sk_confusion_matrix
+import seaborn as sns
+sns.set()
+
+
+#TODO: Write tests :)
+def confusion_matrix(ytrue, ypred, labels=None, normalize=True, percent=True, size=20, fmt='.0f'):
+    """
+    Render the confusion matrix using seaborn.
+    Labels are taken from ytrue values if not given as parameters.
+    """
+    if labels is None:
+        labels = sorted(np.unique(ytrue))
+
+    cm = sk_confusion_matrix(ytrue, ypred, labels=labels)
+
+    if normalize:
+        cm = cm.astype(float) / cm.sum(axis=1)[:, np.newaxis]
+    if percent:
+        cm = cm * 100.0
+
+    plt.figure(figsize=(size, size))
+    ax = sns.heatmap(cm, annot=True, fmt=fmt, linewidths=.5, cbar=False)
+    ax.xaxis.tick_top()
+    ax.set_xticklabels(labels)
+    ax.set_yticklabels(labels[::-1])
+    ax.set_title("Confusion Matrix", y=1.03, size="x-large", weight='bold')
+    ax.set_ylabel("True Label", weight='bold')
+    ax.set_xlabel("Predicted Label", weight='bold')
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bblearn',
+      version='0.1',
+      description='Machine Learning made easier, at least for me',
+      url='https://github.com/kenoph/bblearn',
+      author='Paolo Montesel',
+      license='MIT',
+      packages=['bblearn'],
+      zip_safe=False)
+
